69ea9739f0f97cca7b4e89d0432adc6700fbf89e
happydelly <1276443804@qq.com>
2019-12-09 14:27:37 +0800

    第二次提交,新增test2

--- src/main/java/com/ttx/demo/test2.java
+++ src/main/java/com/ttx/demo/test2.java
@@ -1,6 +1,7 @@
 package com.ttx.demo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,6 +10,9 @@ public class test2 {
     public static void main(String[] args){
         List<prodcutVo> prod = new ArrayList<>();
         List<prodcutVo> prod2 = new ArrayList<>();
+
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
         prodcutVo p1 = new prodcutVo("1","红色");
         prodcutVo p2 = new prodcutVo("2","黄色");
         prodcutVo p3 = new prodcutVo("3","红色");
@@ -21,6 +25,9 @@ public class test2 {
 
         List<String> strings = prod.stream().map(prodcutVo::getColor)
                 .collect(Collectors.toList());
+        Integer count = list.stream().reduce(0,(x,y)->x+y);
+
+        System.out.println(count);
         System.out.println(prod2);
         System.out.println(strings);
     }
