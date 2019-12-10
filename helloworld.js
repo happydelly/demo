e1ee2dc7bfb647d7c915dbcb7c906d306f075a5a
happydelly <1276443804@qq.com>
2019-12-10 09:42:42 +0800

    新增helloworld.js

---
+++ helloworld.js
@@ -0,0 +1,59 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+
+    // 发送 HTTP 头部
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // 发送响应数据 "Hello World"
+    response.end('Hello World\n');
+}).listen(8888);
+
+var fs = require("fs");
+
+fs.readFile('input.txt', function (err, data) {
+    if (err) return console.error(err);
+    console.log(data.toString());
+});
+
+console.log("程序执行结束!");
+
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+// 监听器 #1
+var listener1 = function listener1() {
+    console.log('监听器 listener1 执行。');
+}
+
+// 监听器 #2
+var listener2 = function listener2() {
+    console.log('监听器 listener2 执行。');
+}
+
+// 绑定 connection 事件，处理函数为 listener1
+eventEmitter.addListener('connection', listener1);
+
+// 绑定 connection 事件，处理函数为 listener2
+eventEmitter.on('connection', listener2);
+
+var eventListeners = eventEmitter.listenerCount('connection');
+console.log(eventListeners + " 个监听器监听连接事件。");
+
+// 处理 connection 事件
+eventEmitter.emit('connection');
+
+// 移除监绑定的 listener1 函数
+eventEmitter.removeListener('connection', listener1);
+console.log("listener1 不再受监听。");
+
+// 触发连接事件
+eventEmitter.emit('connection');
+
+eventListeners = eventEmitter.listenerCount('connection');
+console.log(eventListeners + " 个监听器监听连接事件。");
+
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
